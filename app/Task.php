0223b82c7da0c83417ef1365fa16220daa842f18
denlyou <denlyou85@gmail.com>
2017-08-24 11:21:47 +0900

    만료일(due_date) 추가

--- app/Task.php
+++ app/Task.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = [
+      'name',
+      'due_date' // 완료일
+    ];
 
     public function user()
     {
@@ -19,5 +22,21 @@ class Task extends Model
         $formattedDate = Carbon::createFromFormat("Y-m-d H:i:s", $task->created_at);
         $format = "Y-m-d H:i";
         return $formattedDate->format($format);
+    }
+
+    /* D-Day 구하기 */
+    public function getDdays( $now )
+    {
+        date_default_timezone_set('Asia/Seoul'); // 한국시간 문제
+        // return ;
+        $result = "UNLIMIT";
+        if ( isset($this->due_date) ) {
+          $now_date = date('Y-m-d', $now);
+          $due_date = substr($this->due_date, 0,10);
+          if ( $now_date == $due_date ) return "D-Day";
+          $_days = (strtotime($due_date.' 00:00:00')-strtotime($now_date.' 00:00:00')) / 86400 ; // 24*60*60
+          $result = 'D'.( ($_days < 0)?'+':'-' ).abs($_days);
+        }
+        return $result;
     }
 }
