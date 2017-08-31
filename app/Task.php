2452e0ce7f078fdedaa3d19e7fc8b98bbb7f83ad
YJKwak <devops@osen.co.kr>
2017-08-31 15:45:24 +0900

    task 모델에 태그 패키지 사용 위한 trait 정의
    https://github.com/rtconner/laravel-tagging

--- app/Task.php
+++ app/Task.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Carbon\Carbon;
+use Conner\Tagging\Taggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    use Taggable;
+
     protected $fillable = ['name'];
 
     public function user()
@@ -19,5 +22,16 @@ class Task extends Model
         $formattedDate = Carbon::createFromFormat("Y-m-d H:i:s", $task->created_at);
         $format = "Y-m-d H:i";
         return $formattedDate->format($format);
+    }
+
+    public function getTagNamesToCsv(Task $task)
+    {
+        if (empty($task->tagNames())) {
+            $result = "";
+        } else {
+            $result = implode(",", $task->tagNames());
+        }
+
+        return $result;
     }
 }
